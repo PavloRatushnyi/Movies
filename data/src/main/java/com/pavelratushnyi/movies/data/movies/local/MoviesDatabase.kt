bba8721f6bc0b6f514f7aa208d6a21115dc2014c
Pavlo Ratushnyi <pavelratushniy@gmail.com>
2023-06-13 22:42:31 +0200

    Set exportSchema to false

--- data/src/main/java/com/pavelratushnyi/movies/data/movies/local/MoviesDatabase.kt
+++ data/src/main/java/com/pavelratushnyi/movies/data/movies/local/MoviesDatabase.kt
@@ -2,7 +2,14 @@ package com.pavelratushnyi.movies.data.movies.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.pavelratushnyi.movies.data.moviedetails.local.*
+import com.pavelratushnyi.movies.data.moviedetails.local.MovieDetailsDao
+import com.pavelratushnyi.movies.data.moviedetails.local.MovieDetailsEntity
+import com.pavelratushnyi.movies.data.moviedetails.local.MovieGenreCrossRef
+import com.pavelratushnyi.movies.data.moviedetails.local.MovieGenreEntity
+import com.pavelratushnyi.movies.data.moviedetails.local.MovieProductionCompanyCrossRef
+import com.pavelratushnyi.movies.data.moviedetails.local.MovieProductionCompanyEntity
+import com.pavelratushnyi.movies.data.moviedetails.local.MovieProductionCountryCrossRef
+import com.pavelratushnyi.movies.data.moviedetails.local.MovieProductionCountryEntity
 
 @Database(
     entities = [
@@ -15,7 +22,8 @@ import com.pavelratushnyi.movies.data.moviedetails.local.*
         MovieProductionCompanyCrossRef::class,
         MovieProductionCountryCrossRef::class,
     ],
-    version = 1
+    version = 1,
+    exportSchema = false,
 )
 internal abstract class MoviesDatabase : RoomDatabase() {
     abstract fun moviesDao(): MoviesDao
