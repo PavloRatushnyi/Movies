1049fc8c03cffed792ea251fc9fa3509524c33ac
Pavlo Ratushnyi <pavelratushniy@gmail.com>
2023-06-13 20:37:27 +0200

    Use dynamic colors

--- app/src/main/java/com/pavelratushnyi/movies/ui/theme/Theme.kt
+++ app/src/main/java/com/pavelratushnyi/movies/ui/theme/Theme.kt
@@ -1,21 +1,29 @@
 package com.pavelratushnyi.movies.ui.theme
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-
-private val DarkColorScheme = darkColorScheme()
-
-private val LightColorScheme = lightColorScheme()
+import androidx.compose.ui.platform.LocalContext
 
 @Composable
 fun MoviesTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colorScheme = if (darkTheme) {
-        DarkColorScheme
+    val colorScheme = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        if (darkTheme) {
+            dynamicDarkColorScheme(LocalContext.current)
+        } else {
+            dynamicLightColorScheme(LocalContext.current)
+        }
     } else {
-        LightColorScheme
+        if (darkTheme) {
+            darkColorScheme()
+        } else {
+            lightColorScheme()
+        }
     }
 
     MaterialTheme(
