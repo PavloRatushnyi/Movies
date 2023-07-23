eae7f0866ec749d1d2d3605704170aa01da2bbd4
Pavlo Ratushnyi <pavelratushniy@gmail.com>
2023-07-23 10:41:48 +0200

    Create MoviesNavHost

--- app/src/main/java/com/pavloratushnyi/movies/ui/MoviesApp.kt
+++ app/src/main/java/com/pavloratushnyi/movies/ui/MoviesApp.kt
@@ -4,12 +4,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
-import com.pavloratushnyi.movies.feature.movie_details.movieDetailsScreen
-import com.pavloratushnyi.movies.feature.movie_details.navigateToMovieDetails
-import com.pavloratushnyi.movies.feature.movies.MOVIES_DESTINATION_ROUTE
-import com.pavloratushnyi.movies.feature.movies.moviesScreen
 import com.pavloratushnyi.movies.ui.theme.MoviesTheme
 
 @Composable
@@ -17,15 +12,9 @@ internal fun MoviesApp() {
     MoviesTheme {
         Surface(modifier = Modifier.fillMaxSize()) {
             val navController = rememberNavController()
-            NavHost(
+            MoviesNavHost(
                 navController = navController,
-                startDestination = MOVIES_DESTINATION_ROUTE
-            ) {
-                moviesScreen(onMovieClicked = { movie ->
-                    navController.navigateToMovieDetails(movie.id)
-                })
-                movieDetailsScreen()
-            }
+            )
         }
     }
 }
---
+++ app/src/main/java/com/pavloratushnyi/movies/ui/MoviesNavHost.kt
@@ -0,0 +1,24 @@
+package com.pavloratushnyi.movies.ui
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import com.pavloratushnyi.movies.feature.movie_details.movieDetailsScreen
+import com.pavloratushnyi.movies.feature.movie_details.navigateToMovieDetails
+import com.pavloratushnyi.movies.feature.movies.MOVIES_DESTINATION_ROUTE
+import com.pavloratushnyi.movies.feature.movies.moviesScreen
+
+@Composable
+internal fun MoviesNavHost(
+    navController: NavHostController,
+) {
+    NavHost(
+        navController = navController,
+        startDestination = MOVIES_DESTINATION_ROUTE
+    ) {
+        moviesScreen(onMovieClicked = { movie ->
+            navController.navigateToMovieDetails(movie.id)
+        })
+        movieDetailsScreen()
+    }
+}
